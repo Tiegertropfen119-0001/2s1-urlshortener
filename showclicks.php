01aeca539f8179ec3fc9121dc0d1d80b4cc5af06
Tiegetropfen119 <46197002+Tiegetropfen119@users.noreply.github.com>
2022-06-27 12:20:12 +0200

    Add files via upload

--- showclicks.php
+++ showclicks.php
@@ -1,12 +1,13 @@
 <html>
   <title>0url</title>
-    
+    <link rel="stylesheet" href="style.css">
 </html>
 <?php
 
 
 
-    $req1 = '{"shortthisurl": "';
+    $urlid = $_POST['lname'];
+    $req1 = '{"urlid": "';
     $req1 .= $_POST['lname'];
     $req1 .= '"}';
     $body = $req1;
@@ -14,25 +15,22 @@
 
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://localhost:9135/urlshort");
+curl_setopt($ch, CURLOPT_URL, "http://localhost:9135/getclicks");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
 $result = curl_exec($ch);
-echo '<link rel="stylesheet" href="style.css">';
-echo '<html>';
-echo '<div class="name">';
-echo '<h1><center> ';
-echo $result;
-echo '</h1></center>';
-echo '</div>';
-echo '</html>';
+echo "<body>";
+
+echo "<strong><center><h1 class='phptext'>$result</h1></center></strong>";
+echo "</body>";
 
 
 
 
 exit();
+
 
 
 
